eb6e156e660bc820cc1b5908b852698f5aa3982d
CassiusCardoso <cassiuscardosoo@gmail.com>
2025-08-06 14:44:48 -0300

    refactor: remove arquivos não relacionados a usuário para isolar feature

--- ecommerce.web/Models/ErrorViewModel.fs
+++
@@ -1,9 +0,0 @@
-namespace ecommerce.web.Models
-
-open System
-
-type ErrorViewModel =
-    { RequestId: string }
-
-    member this.ShowRequestId =
-        not (String.IsNullOrEmpty(this.RequestId))
